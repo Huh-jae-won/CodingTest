1ea22c38340f0c4e653472a45324e12431ffc1a9
Huh-jae-won <dfr9034@naver.com>
2021-04-29 19:57:36 +0900

    update

--- SW_Expert_Academy/SW역량테스트준비 스터디 모임/기본문제모음/Q01221_GNS.java
+++ SW_Expert_Academy/SW역량테스트준비 스터디 모임/기본문제모음/Q01221_GNS.java
@@ -1,9 +1,72 @@
 package 기본문제모음;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
 public class Q01221_GNS {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Q01221_GNS a = new Q01221_GNS();
+		a.solution();
 	}
+	Map<String,Integer> num;
+	public void solution() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int testCase = Integer.parseInt(br.readLine());
+		num = new HashMap<>();
+		num.put("ZRO", 0);
+		num.put("ONE", 1);
+		num.put("TWO", 2);
+		num.put("THR", 3);
+		num.put("FOR", 4);
+		num.put("FIV", 5);
+		num.put("SIX", 6);
+		num.put("SVN", 7);
+		num.put("EGT", 8);
+		num.put("NIN", 9);
+		List<String> list = new ArrayList<>();
+		
+		for(int tc=1 ; tc<=testCase ; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String an = st.nextToken();
+			int N = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0 ; i<N ; i++) {
+				String temp = st.nextToken();
+				list.add(temp);
+			}
+			list.sort(comp);
+			StringBuilder ret = new StringBuilder();
+			for(int i=0 ; i<N ; i++) {
+				if(i<N-1) {
+					ret.append(String.valueOf(list.get(i))+" ");
+				}else {
+					ret.append(String.valueOf(list.get(i)));
+				}
+			}
+			bw.write(an+"\n"+ret+"\n");
+			bw.flush();
+		}
+		bw.close();
+		br.close();
+	}
+	Comparator comp = new Comparator<String>() {
+		@Override
+		public int compare(String o1, String o2) {
+			return num.get(o1)-num.get(o2);
+		}
+		
+	};
 
 }
--- SW_Expert_Academy/src/Solution.java
+++ SW_Expert_Academy/src/Solution.java
@@ -6,15 +6,17 @@ import java.io.OutputStreamWriter;
 
 public class Solution {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Solution a = new Solution();
+		a.solution();
 	}
 	public void solution() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		for(int tc=1 ; tc<=10 ; tc++) {
-
+			bw.write("abc \b d\n");
 		}
+		bw.flush();
 		bw.close();
 		br.close();
 	}
